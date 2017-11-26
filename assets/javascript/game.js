9563b8807829eeba72a50cc3059afd3a979ede53
tannerwwhite <30422707+tannerwwhite@users.noreply.github.com>
2017-11-26 14:20:36 -0700

    revision

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,6 @@
-// Array of possible words for the hangman game
+
 var bands = ["grateful dead",
              "phish",
-             "beauty and the beast",
              "moe",
              "snarky puppy",
              "sound tribe sector nine",
@@ -21,26 +20,24 @@ var lossCount = 0;
 var guesses = 9;
 
 
-// Funtions - Resuable bloicks of code when called upon if needed
-// ===================================================================================
-// Make sure i selected a word
+
 function startGame() {
   bandsWord = bands[Math.floor(Math.random() * bands.length)];
 
-  // Need to break this word apart. Allow me to have an array of individual letters.
+
   letterArray = bandsWord.split("");
 
-  // Need to get Number of blanks required for this word.
+
   numBlanks = letterArray.length;
 
-  // Reset - Need to reset a few key Variables becasue the ones at the top only run for the first round.
+
   guessesLeft = 9;
   wrongLetters = [];
   blanksAndSuccesses = [];
 
-  // Populate blanks and success with right number of blanks.
+
   for (var i = 0; i < numBlanks; i++) {
-          // Create a space for any spaces between words
+
     if (bandsWord.charAt(i) === ' ')
     {
       blanksAndSuccesses[i] = "&nbsp;";
@@ -52,22 +49,21 @@ function startGame() {
     }
   }
 
-  // Change HTML to reflect round conditions
-  // Use the .join to get rid of commas between each letter in the HTML
+
   document.getElementById("current-bands").innerHTML = blanksAndSuccesses.join(" ");
 
 
 
-  // Always be Saving / Testing / Debugging
+
   console.log(bandsWord);
   console.log(letterArray);
   console.log(numBlanks);
   console.log(blanksAndSuccesses);
 }
 
-// Compare letter that we type to a letter in the game - REMEMBER because you passed in and called the function letter that you have to run that down in Register Keyclick Section
+
 function checkLetters(letter) {
-  // check if letter exist anywhere in the word
+
   var isLetterInWord = false;
   for (var i = 0; i < numBlanks; i++) {
       if (bandsWord[i] == letter) {
@@ -75,7 +71,7 @@ function checkLetters(letter) {
       }
   }
 
-  // Check where in word the letter exists, then popluate out blanksAndSuccesses array.
+
   if(isLetterInWord) {
   for (var i = 0; i<numBlanks; i++) {
       if(bandsWord[i] == letter) {
@@ -83,42 +79,41 @@ function checkLetters(letter) {
       }
   }
 }
-  // Letter wasnt found
+
   else {
       wrongLetters.push(letter);
       guessesLeft--
   }
 
-  // Always be Saving / Testing / Debugging
+
   console.log(blanksAndSuccesses);
 
 }
-  // Runs after each round. Reduces guess increase wins..
+
   function roundComplete() {
       console.log("Win Count: " + winCount + " | Loss Count: " + lossCount + " | Guesses Left: " + guessesLeft);
 
-      // Update the HTML to reflect the most recent count stats
+
       document.getElementById("guess-remain").innerHTML = guessesLeft;
       document.getElementById("current-bands").innerHTML = blanksAndSuccesses.join(" ");
       document.getElementById("letters-guessed").innerHTML = wrongLetters.join(" ");
 
-      // Check if user won
-      // If all letters in the blanks and Successes match then update winCount
+
       if (letterArray.toString() == blanksAndSuccesses.toString()) {
           winCount++;
           alert("You love shitty music!");
 
-          // Update the win counter in the HTML
+
           document.getElementById("winCounter").innerHTML = winCount;
 
           startGame();
       }
-      // Check if user lost
+
       else if (guessesLeft == 0) {
           lossCount++
-          alert("You lost!");
+          alert("You lose!");
 
-          // Update the HTML
+
           document.getElementById("lossCounter").innerHTML = lossCount;
 
           startGame();
@@ -130,17 +125,15 @@ function checkLetters(letter) {
 
 
 
-// MAIN PROCESS - Call upon the functions to actually make something happen
-// ===================================================================================
-// Initiates the code the first time
-startGame(); // Need this to run the function.
 
-// Register keyclicks
+startGame();
+
+
 document.onkeyup = function (event) {
   var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
   checkLetters(letterGuessed);
   roundComplete();
 
-  // Always be Saving / Testing / Debugging
+
   console.log(letterGuessed);
 }
